74cef68a93a70ff269078de678217d4301fde4ed
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2023-10-16 16:45:27 -0700

    add flash options

--- init.lua
+++ init.lua
@@ -369,7 +369,49 @@ local plugins = {
         -- ???
         'folke/flash.nvim',
         event = 'VeryLazy',
-        opts = {}
+        opts = {
+            label = {
+                -- only use lowercase letters for jump labels
+                uppercase = false,
+                -- use <CR> as the first label
+                current = false,
+                -- position the labels at the beginning of the search words
+                after = false,
+                before = true,
+            },
+            highlight = {
+                backdrop = false,
+            },
+            modes = {
+                search = {
+                    enabled = false,
+                },
+                char = {
+                    autohide = true,
+                    search = {
+                        wrap = true,
+                    },
+                    highlight = {
+                        backdrop = false,
+                        matches = false,
+                        groups = {
+                            label = "FlashMatch",
+                        },
+                    },
+                },
+            },
+            remote_op = {
+                motion = true,
+            },
+        },
+        keys = {
+            -- use C-f to activate flash during normal mode, operator pending mode, and visual mode
+            { "<C-f>", mode = { "n", "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+            --{ "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            -- use r to activate flash in operator pending mode
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            --{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        },
     },
 }
 require("lazy").setup(plugins, {})
