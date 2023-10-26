1d193b31455d39e4d08cd2c506d9c0eb69fd39f6
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2023-10-26 10:14:15 -0700

    fix drop config

--- .config/nvim/lua/plugins/drop.lua
+++ .config/nvim/lua/plugins/drop.lua
@@ -2,19 +2,15 @@ return function()
     return {
         'folke/drop.nvim',
         config = function ()
-            vim.api.nvim_create_autocmd({'VimEnter'}, {pattern = '*', callback =
-                function()
-                    require('drop').setup({
-                        theme = {
-                            symbols = {"*", "."},
-                            colors = {"blue", "lightblue", "darkblue", "white", "lightgray"}
-                        },
-                        max = 170,
-                        interval = 150,
-                        screensaver = 1000 * 10 * 60,
-                        filetypes = {},
-                    })
-                end
+            require('drop').setup({
+                theme = {
+                    symbols = {"*", "."},
+                    colors = {"blue", "lightblue", "darkblue", "white", "lightgray"}
+                },
+                max = 170,
+                interval = 150,
+                screensaver = 1000 * 10 * 60,
+                filetypes = {},
             })
         end,
         event = 'VeryLazy',
