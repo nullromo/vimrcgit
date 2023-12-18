d1497f2245e92d4ec3652a37210109096df01217
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2023-12-18 14:21:23 -0800

    use cash plugin

--- .config/nvim/lua/plugins/cash.lua
+++ .config/nvim/lua/plugins/cash.lua
@@ -1,9 +1,15 @@
 return function()
     return {
         'nullromo/cash.nvim',
+        branch = 'dev',
         config = function()
             local cash = require('cash')
             cash.setup()
+
+            -- use <Leader>v to print debug info
+            vim.keymap.set('n', '<Leader>v', function()
+                cash.printDebugInfo()
+            end)
         end,
     }
 end
