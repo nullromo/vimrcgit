2a39a966c258ff1c391dd3e54c94ebb768ea07ef
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2024-05-20 12:14:28 -0700

    update gitlinker config

--- .config/nvim/lua/plugins/gitlinker.lua
+++ .config/nvim/lua/plugins/gitlinker.lua
@@ -3,20 +3,25 @@ return function()
         'ruifm/gitlinker.nvim',
         config = function()
             require('gitlinker').setup({
-                -- TODO: disable default mapping doesn't work (see https://github.com/ruifm/gitlinker.nvim/issues/48)
+                callbacks = {
+                    ['gitlab.diconcloud.com'] = require('gitlinker.hosts').get_gitlab_type_url,
+                },
+                -- TODO: disabling the default mapping doesn't actually work (see https://github.com/ruifm/gitlinker.nvim/issues/48)
                 mappings = nil,
             })
+
+            -- Use <leader>gb to open a browser link to the current line
             vim.api.nvim_set_keymap(
                 'n',
                 '<leader>gb',
                 "<cmd>lua require('gitlinker').get_buf_range_url('n', {action_callback = require('gitlinker.actions').open_in_browser})<cr>",
-                { silent = true }
+                { silent = true, desc = 'Gitlinker from normal mode' }
             )
             vim.api.nvim_set_keymap(
                 'v',
                 '<leader>gb',
                 "<cmd>lua require('gitlinker').get_buf_range_url('v', {action_callback = require('gitlinker.actions').open_in_browser})<cr>",
-                {}
+                { desc = 'Gitlinker from visual mode' }
             )
         end,
     }
