1719b5e3f9be91e192b0ad5ad9c717af8eb4c489
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2025-04-30 15:13:26 -0700

    update register editor plugin

--- .config/nvim/lua/plugins/registereditor.lua
+++ .config/nvim/lua/plugins/registereditor.lua
@@ -1,6 +1,10 @@
 return function()
     return {
+        -- this is a fork of tuurep/registereditor. See
+        -- https://github.com/tuurep/registereditor/pull/6 and
+        -- https://github.com/tuurep/registereditor/pull/7
         'nullromo/registereditor',
+        branch = 'multiple-edit',
         config = function()
             local registereditor = require('registereditor')
             registereditor.setup()
