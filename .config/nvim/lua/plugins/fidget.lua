71461b9502e0fcf1ea0ad25bacd0ea557db316e9
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2025-11-06 10:04:28 -0800

    remove user commands that I don't want

--- .config/nvim/lua/plugins/fidget.lua
+++ .config/nvim/lua/plugins/fidget.lua
@@ -6,6 +6,9 @@ return function()
         event = 'LspAttach',
         config = function()
             require('fidget').setup()
+
+            -- I never use the :Fidget command and it interferes with :F<Tab>
+            vim.api.nvim_del_user_command('Fidget')
         end,
     }
 end
--- .config/nvim/lua/plugins/nvim-regexplainer.lua
+++ .config/nvim/lua/plugins/nvim-regexplainer.lua
@@ -14,6 +14,16 @@ return function()
                 display = 'popup',
                 --mappings = { toggle = 'gR' },
             })
+
+            -- I don't use these commands and they interfere with :R<Tab>
+            vim.api.nvim_del_user_command('RegexplainerClearCache')
+            vim.api.nvim_del_user_command('RegexplainerDebug')
+            vim.api.nvim_del_user_command('RegexplainerHide')
+            vim.api.nvim_del_user_command('RegexplainerShow')
+            vim.api.nvim_del_user_command('RegexplainerShowPopup')
+            vim.api.nvim_del_user_command('RegexplainerShowSplit')
+            vim.api.nvim_del_user_command('RegexplainerToggle')
+            vim.api.nvim_del_user_command('RegexplainerYank')
         end,
     }
 end
