cf89abec40f55e37d98d2850369fdfe9e4f8707c
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2026-05-21 13:56:21 -0700

    switch to neovim supported black plugin

--- .config/nvim/lua/plugins/black.lua
+++ .config/nvim/lua/plugins/black.lua
@@ -1,15 +1,23 @@
 -- autoformatting for python
 
+-- Note: to get this to work, you have to follow these steps
+--   mkdir -p ~/.local/venv && cd ~/.local/venv
+--   python3 -m venv nvim
+--   cd nvim
+--   . ./bin/activate
+--   pip install pynvim black
 return function()
     return {
-        'psf/black',
+        'averms/black-nvim',
         init = function()
             vim.g.black_skip_magic_trailing_comma = 1
+            vim.g.python3_host_prog = os.getenv('HOME')
+                .. '/.local/venv/nvim/bin/python'
         end,
         config = function()
             vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
                 pattern = '*.py',
-                command = "execute ':Black'",
+                command = 'call BlackSync()',
                 desc = 'format python files on save',
             })
         end,
