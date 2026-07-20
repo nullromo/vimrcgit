478a5290a1f745c849d76bad47578a54c4f2a77d
Kyle Kovacs <kkovacs@diconfiberoptics.com>
2026-07-20 10:16:55 -0700

    create notification when swapping windows with vim-windowswap

--- .config/nvim/lua/plugins/vim-windowswap.lua
+++ .config/nvim/lua/plugins/vim-windowswap.lua
@@ -8,19 +8,29 @@ return function()
             vim.g.windowswap_map_keys = 0
         end,
         config = function()
+            -- keep track of marked buffer number
+            local markedBuffer = nil
+
             -- Use ,w to 'yank' and 'paste' windows
-            vim.keymap.set(
-                'n',
-                '<Leader>w',
-                ':call WindowSwap#EasyWindowSwap()<CR>',
-                { silent = true, desc = 'window swap' }
-            )
+            vim.keymap.set('n', '<Leader>w', function()
+                -- track marked buffer
+                if markedBuffer == nil then
+                    markedBuffer = vim.api.nvim_get_current_buf()
+                    vim.notify('Yanked buffer ' .. markedBuffer)
+                else
+                    vim.notify(
+                        'Swapped buffers '
+                            .. markedBuffer
+                            .. ' and '
+                            .. vim.api.nvim_get_current_buf()
+                    )
+                    markedBuffer = nil
+                end
+
+                -- call windowswap
+                vim.cmd(':call WindowSwap#EasyWindowSwap()')
+            end, { silent = true, desc = 'window swap' })
         end,
-        keys = {
-            {
-                '<Leader>w',
-                mode = 'n',
-            },
-        },
+        keys = { { '<Leader>w', mode = 'n' } },
     }
 end
